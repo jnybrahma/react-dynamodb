27f79cf30feeaf4baa92a47f91033ae51285b8e4
Brahma, Jnyananjoy <jnybrahma@gmail.com>
2023-04-12 17:44:37 +1000

    first

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         React - AWS Serverless - App
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         React AWS - Amplify
         </a>
       </header>
     </div>
