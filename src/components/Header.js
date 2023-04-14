ac714692c8d42ec52734990096fcc2b5afac318b
Brahma, Jnyananjoy <jnybrahma@gmail.com>
2023-04-14 17:02:28 +1000

    first

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import menuLinksData from './data/menu_links.json'
+import React from 'react'; import menuLinksData from './data/menu_links.json' 
 
-const Header = () => {
-  return (
-    <header id="intro">
-      <article className="fullheight">
+const Header = () => { 
+
+return (
+<header id="intro">
+    <article className="fullheight">
         <div className="hgroup">
-          <h1>Landon Hotel</h1>
-          <h2>West London</h2>
-          <p><a href="#welcome"><img src="https://landonhotel.com/images/misc/arrow.png" alt="down arrow" /></a></p>
+            <h1>Landon Hotel Demo Website</h1>
+            <h2>West London</h2>
+            <p>
+                <a href="#welcome"><img src="https://landonhotel.com/images/misc/arrow.png" alt="down arrow" /></a>
+            </p>
         </div>
-      </article>
+    </article>
 
-      <nav id="nav">
+    <nav id="nav">
         <div className="navbar">
-          <div className="brand"><a href="#welcome">Landon <span>Hotel</span></a></div>
-          <ul>
-            {
-              menuLinksData.map((link) =>
+            <div className="brand"><a href="#welcome">Landon <span>Hotel</span></a></div>
+            <ul>
+                { menuLinksData.map((link) =>
                 <li><a className={`icon ${link.class}`} href={link.href}><span>{link.text}</span></a></li>
-              )
-            }
-          </ul>
+                ) }
+            </ul>
         </div>
-      </nav>
-    </header>
-  );
+    </nav>
+</header>
+); 
 }
-
-export default Header;
+ export default Header;
